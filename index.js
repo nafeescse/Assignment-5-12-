444690b6936c53c5a9cc63578124499265cbe947
nafeescse <abdur17@student.sust.edu>
2025-08-29 11:28:24 +0600

    clipboard copy done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // function for calling service
+
 function callService(name, number) {
   let coin = document.getElementById("coin").innerText;
   let coinNumber = Number(coin);
@@ -44,7 +45,8 @@ function callService(name, number) {
 let heartCount = 0;
 const heart = document.getElementsByClassName("heart-button");
 for (let i = 0; i < heart.length; i++) {
-  heart[i].addEventListener("click", function () {
+  heart[i].addEventListener("click", function (e) {
+    e.preventDefault();
     Swal.fire({
       icon: "success",
       title: "Lovely!",
@@ -57,32 +59,51 @@ for (let i = 0; i < heart.length; i++) {
   });
 }
 
-document.getElementById("all").addEventListener("click", function () {
+// copy counting
+let copyCount = 0;
+const copy = document.getElementsByClassName("copy-button");
+for (let i = 0; i < copy.length; i++) {
+  copy[i].addEventListener("click", function () {
+    copyCount++;
+    document.getElementById("copy").innerText = copyCount;
+  });
+}
+
+document.getElementById("all").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("emergency-service", "emergency-number");
 });
-document.getElementById("police").addEventListener("click", function () {
+document.getElementById("police").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("police-service", "police-number");
 });
-document.getElementById("fire").addEventListener("click", function () {
+document.getElementById("fire").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("fire-service", "fire-number");
 });
-document.getElementById("health").addEventListener("click", function () {
+document.getElementById("health").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("ambulance-service", "ambulance-number");
 });
-document.getElementById("help").addEventListener("click", function () {
+document.getElementById("help").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("women-service", "women-number");
 });
 
-document.getElementById("govt").addEventListener("click", function () {
+document.getElementById("govt").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("anti-service", "anti-number");
 });
-document.getElementById("electricity").addEventListener("click", function () {
+document.getElementById("electricity").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("electricity-service", "electricity-number");
 });
-document.getElementById("ngo").addEventListener("click", function () {
+document.getElementById("ngo").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("brac-service", "brac-number");
 });
-document.getElementById("travel").addEventListener("click", function () {
+document.getElementById("travel").addEventListener("click", function (e) {
+  e.preventDefault();
   callService("railway-service", "railway-number");
 });
 
@@ -93,3 +114,32 @@ document.getElementById("clear-history").addEventListener("click", function () {
   if (!historyContainer) return console.error("No history-container found!");
   historyContainer.innerHTML = "";
 });
+
+// Copy to clipboard
+
+function copyToClipboard(buttonId, targetId) {
+  const btn = document.getElementById(buttonId);
+  const hotLine = document.getElementById(targetId);
+  btn.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(hotLine.textContent)
+      .then(() =>
+        Toastify({
+          text: "Copied to Clipboard",
+          duration: 5000,
+          gravity: "bottom",
+          position: "right",
+        }).showToast()
+      )
+      .catch((err) => console.error("Failed to copy:", err));
+  });
+}
+copyToClipboard("copyEmergencyBtn", "emergency-number");
+copyToClipboard("copyPoliceBtn", "police-number");
+copyToClipboard("copyFireBtn", "fire-number");
+copyToClipboard("copyAmbulanceBtn", "ambulance-number");
+copyToClipboard("copyWomenBtn", "women-number");
+copyToClipboard("copyAntiBtn", "anti-number");
+copyToClipboard("copyElectricityBtn", "electricity-number");
+copyToClipboard("copyBracBtn", "brac-number");
+copyToClipboard("copyRailwayBtn", "railway-number");
